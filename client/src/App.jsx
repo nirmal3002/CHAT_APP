7fda21c67867eac96f6981cf26be8cbb61c18a9e
Your Name <you@example.com>
2026-07-02 11:47:22 +0530

    the updated frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
+import bgImage from './assets/bgImage.svg'
+
+const App = () => {
+  return (
+    <div
+      className="bg-contain bg-center min-h-screen"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
